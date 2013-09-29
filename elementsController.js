be72d5a3b94bd0a18df7e4a0f35fe6c2c2db5ce1
hansmaad <hansmaad@github.com>
2013-09-29 17:57:12 +0200

    Prototypes

--- elementsController.js
+++ elementsController.js
@@ -1,17 +1,54 @@
 
+function Bun(name) {
+  this.type = "bun";
+  this.name = name === undefined ? "Bun" : name;  
+  this.clone = function() { return new Bun(this.name); }
+};
+
+function Meat(name) {
+  this.type = "meat";
+  this.name = name === undefined ? "Meat" : name;  
+  this.clone = function() { return new Meat(this.name); }
+};
+
+
+function Cheese(name) {
+  this.type = "cheese";
+  this.cheeseType = undefined;
+  this.name = name === undefined ? "Cheese" : name;
+  this.clone = function() { return new Cheese(this.name); }
+  this.types = [ "cheddar", "gauda" ];
+};
+
+
 
 function ElementsController($scope) {
     $scope.prototypes = [
-        "Bun", "Meat", "Tomato", "Onions", "Chili", "Bacon"
+        new Bun(),
+        new Meat(), 
+        new Meat("Meat (beef)"),
+        new Meat("Meat (pork)"),
+        new Meat("Meat (chicken)"),
+        new Cheese(),
+        new Cheese("Cheese (cheddar)"),
+        new Cheese("Cheese (gauda)"),
     ];
 
     $scope.elements = [ 
-        { name : "Bun" }, 
-        { name : "Meat" }
+        { name : "Bun" },
     ];
-    
-    $scope.addElement = function() {
-        $scope.elements.push({ name : $scope.newElement });
-    }
+   
+    $scope.newElement = undefined;
 
+    $scope.addElement = function(index) {
+        var length = $scope.elements.length;
+        if (index === undefined)
+          index = 0;
+        
+        var reverseIndex = length - index;
+                   
+       $scope.elements.splice(reverseIndex, 0, $scope.newElement.clone());
+        $scope.newElement = "";
+    }
+    
 }
