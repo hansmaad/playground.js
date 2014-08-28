8783a1864d41e7677da0aa1c6f6f0fe63ad85da9
hansmaad <hansmaad@gmail.com>
2014-08-28 16:53:22 +0200

    lv-table

---
+++ lv-table.js
@@ -0,0 +1,37 @@
+angular.module("lvTable", [])
+  .directive("lvTableCell", function($compile) {
+    return {
+      restrict : "A",
+      link : function(scope, elem, attr) {
+        var template = scope.col.template || "{{ row[col.field] }}";
+        if (template[0] !== "<")
+          template = "<span>" + template + "</span>"
+        var newElem = $compile(template)(scope);
+        elem.contents().remove();
+        elem.append(newElem);
+      }
+    };
+    
+    
+  })
+  .directive("lvTable", function($compile) {
+    var makeColumns = function(data) {
+      var columns = [];
+      angular.forEach(data, function(value, key) {
+        columns.push({ field: key, displayName: key});
+      });
+      return columns;
+    };
+    return {
+      restrict : "EA",
+      templateUrl : "lv-table.html",
+      replace : true,
+      transclude : true,
+      scope : {
+        lvTable : "="
+      },
+      link : function(scope, elem, attr) {
+        scope.columns = scope.lvTable.columns || makeColumns(scope.lvTable.data[0]);
+      }
+    };
+  });
